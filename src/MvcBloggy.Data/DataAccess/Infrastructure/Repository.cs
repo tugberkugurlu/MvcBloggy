e28bf382f229966d8d8bf2587da3c016a18d2d9f
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2011-10-13 21:44:28 +0300

    initial third commit

---
+++ src/MvcBloggy.Data/DataAccess/Infrastructure/Repository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+namespace MvcBloggy.Data.DataAccess.Infrastructure {
+
+    public abstract class Repository<T, C> : IRepository<T, C> where T : class where C : DbContext, new() {
+
+        private C _entities = new C();
+
+        public C Context {
+
+            get { return _entities; }
+            set { _entities = value; }
+        }
+
+        public IQueryable<T> FindBy(Expression<Func<T, bool>> predicate) {
+
+            IQueryable<T> query = _entities.Set<T>().Where(predicate);
+            return query;
+        }
+
+        public void Add(T entity) {
+            _entities.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity) {
+            _entities.Set<T>().Remove(entity);
+        }
+
+        public void Edit(T entity) {
+            _entities.Entry(entity).State = System.Data.EntityState.Modified;
+        }
+
+        public void Save() {
+            _entities.SaveChanges();
+        }
+    }
+}
---
+++ src/MvcBloggy.Data/DataAccess/SqlServer/Model/BlogPostTag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcBloggy.Data.DataAccess.SqlServer.Model {
+
+    public class BlogPostTag {
+
+        public string Tag { get; set; }
+        public int Count { get; set; }
+    }
+}
