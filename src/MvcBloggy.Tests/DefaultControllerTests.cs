1f518db1e5a8464d69fc604b64801bb2ef46501c
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-04-02 17:52:44 +0300

    updated mvc 4 dlls, removed bin deployment folder

---
+++ src/MvcBloggy.Tests/DefaultControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace MvcBloggy.Tests {
+
+    public class DefaultControllerTests {
+
+        [Fact]
+        public void pass_you_son_of_a_bitch_fucking_bastard() {
+
+            Assert.Equal(1, 1);
+        }
+
+        //home page should list the latest 5 posts
+        //the pagination should be implemented
+    }
+}
---
+++ src/MvcBloggy.Web/Application/Services/ITokenHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcBloggy.Web.Application.Services {
+
+    public interface ITokenHandler {
+        
+        void SetClientAccess(string token);
+        void RemoveClientAccess();
+        string GetToken();
+    }
+}
