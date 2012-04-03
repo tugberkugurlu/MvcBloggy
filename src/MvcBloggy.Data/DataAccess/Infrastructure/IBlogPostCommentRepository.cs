eb870753a0dca207be96c090ace570a9c2f5b865
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-04-03 22:02:22 +0300

    added GenericRepository package and modified some code on data project

--- src/MvcBloggy.Data/DataAccess/Infrastructure/IBlogPostCommentRepository.cs
+++ src/MvcBloggy.Data/DataAccess/Infrastructure/IBlogPostCommentRepository.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
+using GenericRepository;
 using MvcBloggy.Data.DataAccess.SqlServer;
 
 namespace MvcBloggy.Data.DataAccess.Infrastructure {
 
-    public interface IBlogPostCommentRepository : IGenericRepository<BlogPostComment> {
+    public interface IBlogPostCommentRepository : IRepository<BlogPostComment> {
 
     }
 }
--- src/MvcBloggy.Data/DataAccess/Infrastructure/IBlogPostRepository.cs
+++ src/MvcBloggy.Data/DataAccess/Infrastructure/IBlogPostRepository.cs
@@ -5,21 +5,19 @@ using System.Linq;
 using System.Text;
 using MvcBloggy.Data.DataAccess.SqlServer;
 using System.Linq.Expressions;
+using GenericRepository;
 
 namespace MvcBloggy.Data.DataAccess.Infrastructure {
 
     //Haven't found a way to mock the DbContext. So, I am skiping that part for now.
     //public interface IBlogPostRepository<C> : IRepository<BlogPost, C> where C : DbContext {
 
-    public interface IBlogPostRepository : IGenericRepository<BlogPost> {
+    public interface IBlogPostRepository : IRepository<BlogPost> {
 
         IEnumerable<BlogPost> GetAll(ApprovalStatus approvalStatus = ApprovalStatus.All);
         IEnumerable<BlogPost> GetAll(int languageID, ApprovalStatus approvalStatus = ApprovalStatus.All);
         IEnumerable<BlogPost> GetAll(string tag, ApprovalStatus approvalStatus = ApprovalStatus.All);
         IEnumerable<BlogPost> GetAll(string[] tags, ApprovalStatus approvalStatus = ApprovalStatus.All);
-        
-        //Not sure if this needs to be implemented or not...
-        //IEnumerable<BlogPost> FindBy(Expression<Func<BlogPost, bool>> predicate);
 
         BlogPost GetSingle(int blogPostID, ApprovalStatus approvalStatus = ApprovalStatus.All);
         BlogPost GetSingle(Guid blogPostGUID, ApprovalStatus approvalStatus = ApprovalStatus.All);
