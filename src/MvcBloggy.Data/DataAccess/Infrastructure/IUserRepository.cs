8d95fd106c41abd0a14406abcc3d4324e0696b32
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-04-06 02:07:48 +0300

    implemented UserRepository

---
+++ src/MvcBloggy.Data/DataAccess/Infrastructure/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GenericRepository;
+using MvcBloggy.Data.DataAccess.SqlServer;
+
+namespace MvcBloggy.Data.DataAccess.Infrastructure {
+
+    public interface IUserRepository : IRepository<User> {
+
+        User GetSingle(string userName, string password);
+    }
+}
---
+++ src/MvcBloggy.Data/DataAccess/SqlServer/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GenericRepository.EF;
+using MvcBloggy.Data.DataAccess.Infrastructure;
+
+namespace MvcBloggy.Data.DataAccess.SqlServer {
+
+    public class UserRepository : Repository<MvcBloggyContext, User>, IUserRepository {
+
+        public User GetSingle(string userName, string password) {
+
+            return All.FirstOrDefault(x =>
+                x.IsApproved == true && x.UserName == userName && x.Password == password
+            );
+        }
+    }
+}
