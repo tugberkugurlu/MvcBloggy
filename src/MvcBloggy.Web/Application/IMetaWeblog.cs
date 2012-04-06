8883abb0690d219bb0118ef3caf337ca439b8b86
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-04-06 03:15:28 +0300

    added MetaWeblogAPI initial structure in a very, very dirty way

---
+++ src/MvcBloggy.Web/Application/IMetaWeblog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using CookComputing.XmlRpc;
+
+namespace MvcBloggy.Web.Application {
+
+    public interface IMetaWeblog {
+
+        [XmlRpcMethod("metaWeblog.newPost")]
+        string AddPost(string blogid, string username, string password, Post post, bool publish);
+
+        [XmlRpcMethod("metaWeblog.editPost")]
+        bool UpdatePost(string postid, string username, string password, Post post, bool publish);
+
+        [XmlRpcMethod("metaWeblog.getPost")]
+        Post GetPost(string postid, string username, string password);
+
+        [XmlRpcMethod("metaWeblog.getCategories")]
+        CategoryInfo[] GetCategories(string blogid, string username, string password);
+
+        [XmlRpcMethod("metaWeblog.getRecentPosts")]
+        Post[] GetRecentPosts(string blogid, string username, string password, int numberOfPosts);
+
+        [XmlRpcMethod("metaWeblog.newMediaObject")]
+        MediaObjectInfo NewMediaObject(string blogid, string username, string password,
+            MediaObject mediaObject);
+
+        [XmlRpcMethod("blogger.deletePost")]
+        [return: XmlRpcReturnValue(Description = "Returns true.")]
+        bool DeletePost(string key, string postid, string username, string password, bool publish);
+
+        [XmlRpcMethod("blogger.getUsersBlogs")]
+        BlogInfo[] GetUsersBlogs(string key, string username, string password);
+
+        [XmlRpcMethod("blogger.getUserInfo")]
+        UserInfo GetUserInfo(string key, string username, string password);
+    }
+}
---
+++ src/MvcBloggy.Web/Services/MetaWeblogAPI.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" Class="MvcBloggy.Web.Application.MetaWeblog" %>
