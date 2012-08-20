ff4e90eab5d97cf5228c9b8e7557f21c18a9de08
tugberkugurlu <tugberkugurlu@gmail.com>
2012-08-20 10:08:01 -0700

    registered repositories inside the IoC container

--- src/MvcBloggy.API/Config/AutofacWebAPI.cs
+++ src/MvcBloggy.API/Config/AutofacWebAPI.cs
@@ -1,7 +1,9 @@
 ﻿using Autofac;
 using Autofac.Integration.WebApi;
+using MvcBloggy.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -22,6 +24,36 @@ namespace MvcBloggy.API.Config {
         private static IContainer RegisterServices(ContainerBuilder builder) {
 
             builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).PropertiesAutowired();
+
+            //DbContext
+            builder.RegisterType<MvcBloggyEntities>()
+                .As<DbContext>().InstancePerApiRequest();
+
+            //Repositories
+            builder.RegisterType<EntityRepository<BlogPostComment>>()
+                .As<IEntityRepository<BlogPostComment>>().InstancePerApiRequest();
+            builder.RegisterType<EntityRepository<BlogPost>>()
+                .As<IEntityRepository<BlogPost>>().InstancePerApiRequest();
+            builder.RegisterType<EntityRepository<BlogPostUrl>>()
+                .As<IEntityRepository<BlogPostUrl>>().InstancePerApiRequest();
+            builder.RegisterType<EntityRepository<BlogRoll>>()
+                .As<IEntityRepository<BlogRoll>>().InstancePerApiRequest();
+            builder.RegisterType<EntityRepository<DynamicPage>>()
+                .As<IEntityRepository<DynamicPage>>().InstancePerApiRequest();
+            builder.RegisterType<EntityRepository<Language>>()
+                .As<IEntityRepository<Language>>().InstancePerApiRequest();
+            builder.RegisterType<EntityRepository<RestrictedPageName>>()
+                .As<IEntityRepository<RestrictedPageName>>().InstancePerApiRequest();
+            builder.RegisterType<EntityRepository<Tag>>()
+                .As<IEntityRepository<Tag>>().InstancePerApiRequest();
+            builder.RegisterType<EntityRepository<TagsForBlogPost>>()
+                .As<IEntityRepository<TagsForBlogPost>>().InstancePerApiRequest();
+            builder.RegisterType<EntityRepository<TagsForDynamicPage>>()
+                .As<IEntityRepository<TagsForDynamicPage>>().InstancePerApiRequest();
+            builder.RegisterType<EntityRepository<User>>()
+                .As<IEntityRepository<User>>().InstancePerApiRequest();
+
+            //Services
 
             return builder.Build();
         }
