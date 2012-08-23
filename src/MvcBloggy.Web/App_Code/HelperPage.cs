9585678677d2c85e90624bfe77db77e07cb6956a
tugberkugurlu <tugberkugurlu@gmail.com>
2012-08-23 12:34:27 -0700

    implemented base viewpage for global helpers so that we can use html specific stuff

---
+++ src/MvcBloggy.Web/App_Code/HelperPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace MvcBloggy.Web.App_Code {
+
+    //http://stackoverflow.com/questions/4710853/using-mvc-htmlhelper-extensions-from-razor-declarative-views
+    public class HelperPage : System.Web.WebPages.HelperPage {
+
+        // Workaround - exposes the MVC HtmlHelper instead of the normal helper
+        public static new HtmlHelper Html {
+
+            get { return ((System.Web.Mvc.WebViewPage)WebPageContext.Current.Page).Html; }
+        }
+    }
+}
