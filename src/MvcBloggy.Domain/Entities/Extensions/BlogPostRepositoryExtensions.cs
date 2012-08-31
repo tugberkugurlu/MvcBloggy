e7afd879aa45a55c1ec90ea566036bbd027a3551
tugberkugurlu <tugberkugurlu@gmail.com>
2012-08-31 13:16:31 +0300

    added GetMonthArchives extension method for blogpostRepository

---
+++ src/MvcBloggy.Domain/Entities/Extensions/BlogPostRepositoryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcBloggy.Domain.Entities {
+    
+    public class BlogPostRepositoryExtensions {
+
+        //ref: http://stackoverflow.com/questions/9886190/entity-framework-efficiently-grouping-by-month
+        public static IEnumerable<MonthArchive> GetMonthArchives(this IEntityRepository<BlogPost> blogPostRepository) {
+
+            return (from blogpost in blogPostRepository.GetAll()
+                    where blogpost.IsApproved == true
+                    group blogpost by new {
+                        Year = blogpost.CreatedOn.Year,
+                        Month = blogpost.CreatedOn.Month
+                    } into g
+                    select new MonthArchive {
+                        Year = g.Key.Year,
+                        Month = g.Key.Month,
+                        Total = g.Sum(x => 1)
+                    }
+            ).AsEnumerable();
+        }
+    }
+}
---
+++ src/MvcBloggy.Domain/Entities/MonthArchive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcBloggy.Domain.Entities {
+    
+    public class MonthArchive {
+
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Total { get; set; }
+    }
+}
